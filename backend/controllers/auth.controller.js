008c8bcc4f2466d58f9e6cc7c27e39d014ddbf8c
Anand P <anandpamegoze@gmail.com>
2024-09-15 23:29:47 +0530

    complete signup, store refreshToken in redis and set cookies with access token and refresh token

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,40 @@
+import { redis } from '../lib/redis.js';
 import User from '../models/user.model.js';
+import jwt from 'jsonwebtoken';
+
+const generateTokens = (userId) => {
+  const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: '15m',
+  });
+  const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: '7d',
+  });
+  return { accessToken, refreshToken };
+};
+
+const storeRefreshToken = async (refreshToken, userId) => {
+  await redis.set(
+    `refresh_token : ${userId}`,
+    refreshToken,
+    'EX',
+    7 * 24 * 60 * 60 //7 days
+  );
+};
+
+const setCookies = (res, accessToken, refreshToken) => {
+  res.cookie('access-token', accessToken, {
+    httpOnly: true, //prevent XSS attacks
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict', //prevents CSRF attacks(cross site request forgery)
+    maxAge: 15 * 60 * 1000,
+  });
+  res.cookie('refresh-token', refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict',
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+};
 
 export async function signup(req, res) {
   try {
@@ -32,7 +68,21 @@ export async function signup(req, res) {
 
     const user = await User.create({ name, email, password });
 
-    res.status(201).json({ user, message: 'User created successfully' });
+    const { accessToken, refreshToken } = generateTokens(user._id);
+
+    await storeRefreshToken(user._id, refreshToken);
+
+    setCookies(res, accessToken, refreshToken);
+
+    res.status(201).json({
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+      message: 'User created successfully',
+    });
   } catch (error) {
     console.log('Error in signup controller : ' + error.message);
     res.status(500).json({ success: false, message: error.message });
