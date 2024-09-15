5cde9c90f91b881e1c9c4c384821155ee665a83f
Anand P <anandpamegoze@gmail.com>
2024-09-15 19:05:35 +0530

    setup redis

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,43 @@
-export const signup = async (req, res) => {
-  res.send('signup');
-};
+import User from '../models/user.model.js';
+
+export async function signup(req, res) {
+  try {
+    const { email, password, name } = req.body;
+    if (!email || !password || !name) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'All fields are required' });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Invalid email format' });
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json({
+        success: false,
+        message: 'Password must be atleast 6 characters',
+      });
+    }
+
+    const existingUserByEmail = await User.findOne({ email: email });
+    if (existingUserByEmail) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Email already exists !' });
+    }
+
+    const user = await User.create({ name, email, password });
+
+    res.status(201).json({ user, message: 'User created successfully' });
+  } catch (error) {
+    console.log('Error in signup controller : ' + error.message);
+    res.status(500).json({ success: false, message: error.message });
+  }
+}
 export const login = async (req, res) => {
   res.send('login');
 };
--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ configDotenv();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+
 app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
