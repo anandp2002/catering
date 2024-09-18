c503cee44a3fb37be51bceb1bd17d1e895b1a56a
Anand P <anandpamegoze@gmail.com>
2024-09-18 14:53:06 +0530

    import functions in cart route

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,5 +1,10 @@
 import express from 'express';
-import { addToCart } from '../controllers/cart.controller.js';
+import {
+  addToCart,
+  getCartProducts,
+  removeAllFromCart,
+  updateQuantity,
+} from '../controllers/cart.controller.js';
 import { protectRoute } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
