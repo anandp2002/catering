9a266a709cc91f056f889c687f53c914d407e3d5
Anand P <anandpamegoze@gmail.com>
2024-09-19 20:18:23 +0530

    coupon route and controller

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import { configDotenv } from 'dotenv';
 import authRoutes from '../backend/routes/auth.route.js';
 import productRoutes from '../backend/routes/product.route.js';
 import cartRoutes from '../backend/routes/cart.route.js';
+import couponRoutes from '../backend/routes/coupon.route.js';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 
@@ -17,6 +18,7 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes);
+app.use('/api/coupons', couponRoutes);
 
 app.listen(PORT, () => {
   console.log('Server is running on http://localhost:' + PORT);
