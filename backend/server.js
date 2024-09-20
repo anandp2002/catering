73d524a105b8bef55c6173360254fadb30696959
Anand P <anandpamegoze@gmail.com>
2024-09-20 15:21:28 +0530

    payment route and controller

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import authRoutes from '../backend/routes/auth.route.js';
 import productRoutes from '../backend/routes/product.route.js';
 import cartRoutes from '../backend/routes/cart.route.js';
 import couponRoutes from '../backend/routes/coupon.route.js';
+import paymentRoutes from '../backend/routes/payment.route.js';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 
@@ -19,6 +20,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/coupons', couponRoutes);
+app.use('/api/payments', paymentRoutes);
 
 app.listen(PORT, () => {
   console.log('Server is running on http://localhost:' + PORT);
