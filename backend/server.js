545e9459ae7d503d0a5475afafe52e934887e8c6
Anand P <anandpamegoze@gmail.com>
2024-09-23 17:18:45 +0530

    admin createProduct

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ configDotenv();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); //Allow us to parse the body of the request
+app.use(express.json({ limit: '10mb' })); //Allow us to parse the body of the request
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
